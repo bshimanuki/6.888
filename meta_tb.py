80c907593e1827a8cdc86520476210add17a8505
Brian Shimanuki <bshimanuki@mit.edu>
2017-11-30 14:26:31 -0500

    activations and pooling

--- meta_tb.py
+++ meta_tb.py
@@ -6,38 +6,79 @@ from fc_os.tb import OSArchTB
 
 import numpy as np
 
-class Conv(object):
-    def __init__(self, image_size, filter_size, in_chn, out_chn, name):
+class Layer(object):
+    def __init__(self, activations):
+        self.activations = activations
+
+    def activation(self, x):
+        for a in self.activations:
+            x = a(x)
+        return x
+
+    def new_shape(self, shape):
+        for a in self.activations:
+            shape = a.new_shape(shape)
+        return shape
+
+class Conv(Layer):
+    def __init__(self, image_size, filter_size, in_chn, out_chn, name, activations=()):
+        super(Conv, self).__init__(activations)
         self.image_size = image_size
         self.filter_size = filter_size
         self.in_chn = in_chn
         self.out_chn = out_chn
         self.name = name
 
-class FC(object):
-    def __init__(self, input_size, output_size, name):
+class FC(Layer):
+    def __init__(self, input_size, output_size, name, activations=()):
+        super(FC, self).__init__(activations)
         self.input_size = input_size
         self.output_size = output_size
         self.name = name
 
-# (TODO): General activations
 class Activation(object):
+    '''Acts on the entire nd array (entire batch).'''
     def __init__(self, func, name):
         self.func = func
         self.name = name
 
-# (TODO): General Pooling
-class Pool(object):
-    def __init__(self, stuff, name):
-        self.name = name
+    def __call__(self, x):
+        return self.func(x)
 
-# (TODO)
-class ReLU(object):
-    def __init__(self, func, name):
+    def new_shape(self, shape):
+        '''Return output shape given input shape.'''
+        return shape
+
+class Pool(Activation):
+    def __init__(self, dh, dw, name):
         self.name = name
+        self.dh = dh
+        self.dw = dw
+
+    def __call__(self, x):
+        return x.reshape((
+            x.shape[0],
+            x.shape[1]//self.dh, self.dh,
+            x.shape[2]//self.dw, self.dw,
+            x.shape[3]
+        )).max(axis=(2,4))
+
+    def new_shape(self, shape):
+        return (
+            shape[0],
+            shape[1]//self.dh,
+            shape[2]//self.dw,
+            shape[3]
+        )
+
+class ReLU(Activation):
+    def __init__(self, name):
+        super(ReLU, self).__init__(self.relu, name)
+
+    def relu(self, x):
+        return np.maximum(x, 0)
 
 class MetaArchTB(Module):
-    # (TODO): allow user to plugin conv weights, biases
     def instantiate(self, arr_x, arr_y, chn_per_word, layers, batch_size):
         self.arr_x = arr_x
         self.arr_y = arr_y
@@ -70,11 +111,12 @@ class MetaArchTB(Module):
                 if cur_image_size is None:
                     pass
                 elif cur_image_size != layer.image_size or cur_in_chn != layer.in_chn:
-                    raise Exception('Invalid conv image size for ' + layer.name)
+                    raise Exception('Invalid conv image size for %s: %s %s' % (layer.name, (cur_image_size, cur_in_chn), (layer.image_size, layer.in_chn)))
                 ifmap_glb_depth, psum_glb_depth, weights_glb_depth = WSArchTB.required_glb_depth(self.arr_x, self.arr_y, self.chn_per_word, layer.image_size, layer.filter_size, layer.in_chn, layer.out_chn)
                 use_conv = True
-                cur_image_size = layer.image_size
-                cur_in_chn = layer.out_chn
+                output_shape = layer.new_shape((None,) + layer.image_size + (layer.out_chn,))
+                cur_image_size = output_shape[1:3]
+                cur_in_chn = output_shape[3]
                 is_conv = True
                 num_convs += 1
             elif isinstance(layer, FC):
@@ -86,7 +128,7 @@ class MetaArchTB(Module):
                     raise Exception('Invalid conv to fc dimension transition to ' + layer.name)
                 ifmap_glb_depth, psum_glb_depth, weights_glb_depth = OSArchTB.required_glb_depth(self.arr_x, self.arr_y, self.chn_per_word, self.batch_size, layer.input_size, layer.output_size)
                 use_fc = True
-                cur_image_size = layer.output_size
+                _, cur_image_size = layer.new_shape((None, layer.output_size))
                 is_conv = False
                 num_fc += 1
             else:
@@ -127,11 +169,13 @@ class MetaArchTB(Module):
                     self.conv_inputs[self.batch_step] = self.conv_tb.get_output()
                     self.batch_step += 1
                     if self.batch_step == self.batch_size:
+                        self.conv_inputs = [batch for batch in old_layer.activation(np.array(self.conv_inputs))]
                         self.batch_step = 0
                         self.layer_step += 1
                         self.cur_conv += 1
                 else:
                     self.fc_input = self.fc_tb.get_output()
+                    self.fc_input = old_layer.activation(self.fc_input)
                     self.layer_step += 1
                     self.cur_fc += 1
                 if self.layer_step == len(self.layers):
--- run.py
+++ run.py
@@ -1,27 +1,31 @@
 from nnsim.simulator import run_tb
-from meta_tb import MetaArchTB, Conv, FC
+from meta_tb import MetaArchTB, Conv, FC, ReLU, Pool
 
 if __name__ == "__main__":
     layers = [
-        Conv(image_size=(4, 4),
+        Conv(image_size=(8, 8),
              filter_size=(3, 3),
              in_chn=3,
              out_chn=6,
              name='conv1',
+             activations=(ReLU('name=relu1'),Pool(dh=2,dw=2,name='pool'))
              ),
         Conv(image_size=(4, 4),
              filter_size=(3, 3),
              in_chn=6,
              out_chn=2,
              name='conv2',
+             activations=(ReLU('name=relu2'),)
              ),
         FC(input_size=32,
            output_size=11,
            name='fc1',
+           activations=(ReLU('name=relu3'),)
            ),
         FC(input_size=11,
            output_size=3,
            name='fc2',
+           activations=(ReLU('name=relu4'),)
            ),
     ]
 
